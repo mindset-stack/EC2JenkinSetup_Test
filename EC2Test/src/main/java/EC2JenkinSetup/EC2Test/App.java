799c88f67d0aa1c005da559dc27892c0557fcb87
vijay.sankpal <vijay.sankpal@blazelcan.com>
2023-03-21 12:56:33 +0530

    First simple hello world comment

---
+++ EC2Test/src/main/java/EC2JenkinSetup/EC2Test/App.java
@@ -0,0 +1,13 @@
+package EC2JenkinSetup.EC2Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ EC2Test/src/test/java/EC2JenkinSetup/EC2Test/AppTest.java
@@ -0,0 +1,20 @@
+package EC2JenkinSetup.EC2Test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
