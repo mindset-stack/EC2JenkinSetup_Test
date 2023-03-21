8377073dd79e2392001a0c1c3026c411dc8e8d40
vijay.sankpal <vijay.sankpal@blazelcan.com>
2023-03-21 13:13:41 +0530

    second commit

--- EC2Test/src/main/java/EC2JenkinSetup/EC2Test/App.java
+++ EC2Test/src/main/java/EC2JenkinSetup/EC2Test/App.java
@@ -1,9 +1,14 @@
 package EC2JenkinSetup.EC2Test;
 
+
+
 /**
  * Hello world!
  *
  */
+
+
+
 public class App 
 {
     public static void main( String[] args )
--- EC2Test/src/test/java/EC2JenkinSetup/EC2Test/AppTest.java
+++ EC2Test/src/test/java/EC2JenkinSetup/EC2Test/AppTest.java
@@ -3,18 +3,22 @@ package EC2JenkinSetup.EC2Test;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.testng.annotations.BeforeTest;
 
 /**
  * Unit test for simple App.
  */
+
+
 public class AppTest 
 {
     /**
      * Rigorous Test :-)
      */
-    @Test
+	@BeforeTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+        System.out.println("Hello");
     }
 }
